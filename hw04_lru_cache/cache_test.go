7e4561f3f4b3dcd7f5a6e0bb63426bed3fe6d13e
666 <666@example.com>
2025-05-13 15:55:49 +0200

    Completed
    completed

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -50,8 +50,90 @@ func TestCache(t *testing.T) {
 	})
 
 	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+		c := NewCache(3)
+
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+		c.Set("ddd", 400)
+
+		c.Clear()
+
+		var lc = c.(*lruCache)
+		require.True(t, len(lc.items) == 0)
+		require.True(t, lc.queue.Len() == 0)
+		require.True(t, lc.capacity == 3)
+
 	})
+	t.Run("getting a place in a place where there is no place", func(t *testing.T) {
+		c := NewCache(3)
+
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+		c.Set("ddd", 400)
+
+		require.True(t, len(c.(*lruCache).items) == 3)
+
+		val, isPresent := c.Get("aaa")
+		require.True(t, isPresent == false)
+		require.True(t, val == nil)
+
+		val, isPresent = c.Get("bbb")
+		require.True(t, isPresent == true)
+		require.True(t, val != nil)
+		require.True(t, val == 200)
+
+		val, isPresent = c.Get("ccc")
+		require.True(t, isPresent == true)
+		require.True(t, val != nil)
+		require.True(t, val == 300)
+
+		val, isPresent = c.Get("ddd")
+		require.True(t, isPresent == true)
+		require.True(t, val != nil)
+		require.True(t, val == 400)
+	})
+
+	t.Run("getting out most old element", func(t *testing.T) {
+		c := NewCache(3)
+
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+
+		c.Get("aaa")
+		c.Get("bbb")
+		c.Get("ccc")
+		c.Get("bbb")
+		c.Set("ccc", 333)
+
+		c.Set("ddd", 400)
+
+		require.Equal(t, 3, c.(*lruCache).queue.Len())
+		require.Equal(t, 3, len(c.(*lruCache).items))
+
+		val, isPresent := c.Get("aaa")
+		require.False(t, isPresent)
+		require.True(t, val == nil)
+
+		val, isPresent = c.Get("bbb")
+		require.True(t, isPresent == true)
+		require.True(t, val != nil)
+		require.True(t, val == 200)
+
+		val, isPresent = c.Get("ccc")
+		require.True(t, isPresent == true)
+		require.True(t, val != nil)
+		require.True(t, val == 333)
+
+		val, isPresent = c.Get("ddd")
+		require.True(t, isPresent == true)
+		require.True(t, val != nil)
+		require.True(t, val == 400)
+
+	})
+
 }
 
 func TestCacheMultithreading(t *testing.T) {
@@ -63,14 +145,14 @@ func TestCacheMultithreading(t *testing.T) {
 
 	go func() {
 		defer wg.Done()
-		for i := 0; i < 1_000_000; i++ {
+		for i := range 1_000_000 {
 			c.Set(Key(strconv.Itoa(i)), i)
 		}
 	}()
 
 	go func() {
 		defer wg.Done()
-		for i := 0; i < 1_000_000; i++ {
+		for range 1_000_000 {
 			c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
 		}
 	}()
