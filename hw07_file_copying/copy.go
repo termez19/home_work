8ff97f30983e4fa473317c6881f41268dfad1096
666 <666@example.com>
2025-06-22 22:03:55 +0200

    done
    done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	pb "github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +16,59 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+
+	sourceInfo, err := os.Stat(fromPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("source file does not exist: %s", fromPath)
+		}
+		return fmt.Errorf("error accessing source file: %w", err)
+	}
+
+	if !sourceInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	fileSize := sourceInfo.Size()
+
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	remainingBytes := fileSize - offset
+
+	bytesToCopy := remainingBytes
+	if limit > 0 && limit < remainingBytes {
+		bytesToCopy = limit
+	}
+
+	sourceFile, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("error opening source file: %w", err)
+	}
+
+	defer sourceFile.Close()
+
+	destFile, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("error creating destination file: %w", err)
+
+	}
+	defer destFile.Close()
+
+	_, err = sourceFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("error seeking source file: %w", err)
+	}
+	// прогресс бар
+	bar := pb.New64(bytesToCopy)
+	bar.SetRefreshRate(time.Millisecond * 100)
+	bar.Start()
+	reader := bar.NewProxyReader(sourceFile)
+	_, err = io.CopyN(destFile, reader, bytesToCopy)
+	if err != nil {
+		return fmt.Errorf("error copying file: %w", err)
+	}
+	bar.Finish()
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -3,5 +3,36 @@ package main
 import "testing"
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	fileName := "testdata/input.txt"
+	to := "testdata/input_copy.txt"
+	err := Copy(fileName, to, 0, 10)
+	if err != nil {
+		t.Errorf("Copy failed: %v", err)
+	}
+}
+func TestOffsetIsGreaterThanFileSize(t *testing.T) {
+	fileName := "testdata/input.txt"
+	to := "testdata/input_copy.txt"
+	err := Copy(fileName, to, 1000000000000000, 10)
+	if err != ErrOffsetExceedsFileSize {
+		t.Errorf("expected ErrOffsetExceedsFileSize, got %v", err)
+	}
+}
+
+func TestUnsupportedFile(t *testing.T) {
+	fileName := "testdata/"
+	to := "testdata/input_copy.txt"
+	err := Copy(fileName, to, 0, 10)
+	if err != ErrUnsupportedFile {
+		t.Errorf("expected ErrUnsupportedFile, got %v", err)
+	}
+}
+
+func TestSourceFileDoesNotExist(t *testing.T) {
+	fileName := "testdata/"
+	to := "testdata/input_copy.txt"
+	err := Copy(fileName, to, 0, 10)
+	if err != ErrUnsupportedFile {
+		t.Errorf("expected ErrUnsupportedFile, got %v", err)
+	}
 }
